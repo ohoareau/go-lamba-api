66d9a2152342e6bb3d1598864bc28dc4e581c052
Olivier Hoareau <oha@greenberets.io>
2021-09-27 10:21:28 +0200

    feat: ability to launch local web server

--- gola.go
+++ gola.go
@@ -2,23 +2,44 @@ package gola
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	runtime "github.com/aws/aws-lambda-go/lambda"
 	"github.com/go-chi/chi/v5"
 	"github.com/ohoareau/gola/adapters"
+	"net/http"
+	"os"
 )
 
 func start(handler ApiGwV2Handler) {
 	runtime.Start(handler)
 }
 
+func startLocal(r Router) {
+	port, hasPort := os.LookupEnv("PORT")
+	if !hasPort {
+		port = "6000"
+	}
+	err := http.ListenAndServe(":" + port, r)
+
+	if nil != err {
+		fmt.Println(err.Error())
+	}
+}
+
+func createRouter(configure RouterConfigurator, features Features) Router {
+	r := chi.NewRouter()
+
+	applyFeatures(r, features)
+
+	configure(r)
+
+	return r
+}
+
 func createHandler(configure RouterConfigurator, features Features) ApiGwV2Handler {
 	return func(event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-		r := chi.NewRouter()
-
-		applyFeatures(r, features)
-
-		configure(r)
+		r := createRouter(configure, features)
 
 		adapter := adapters.CreateChiAdapter(r)
 
@@ -30,5 +51,10 @@ func createHandler(configure RouterConfigurator, features Features) ApiGwV2Handl
 
 //goland:noinspection GoUnusedExportedFunction
 func Gola(configure RouterConfigurator, featurize FeaturesCreator) {
-	start(createHandler(configure, featurize()))
+	_, isLocal := os.LookupEnv("GOLA_LOCAL")
+	if isLocal {
+		startLocal(createRouter(configure, featurize()))
+	} else {
+		start(createHandler(configure, featurize()))
+	}
 }
